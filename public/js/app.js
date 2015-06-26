984344c5e5f5d77a0517052bc4c3ebd359d96a92
Steven Yang <steven@moustacherepublic.com>
2015-06-27 01:34:57 +1200

    minor style fix and storage loading

--- public/js/app.js
+++ public/js/app.js
@@ -41,22 +41,20 @@ angular.module("ampConfig", [])
 app.controller('PlaylistController', ['$scope', 'angularPlayer', '$localStorage', function ($scope, angularPlayer, $localStorage) {
     $scope.volume = 50;
     angularPlayer.adjustVolumeSlider($scope.volume);
-
-    $scope.playlist = $localStorage.playlist;
-    if(typeof(angularPlayer.addTrack) === 'function'){
-        $scope.playlist.forEach(function(item,index){
-            angularPlayer.addTrack(item);
-        });
-    }
-
-    $scope.$on('player:playlist', function(event, data){
-            $localStorage.playlist = data;
-    });
-
     $scope.$watch("volume", function(newValue, oldValue){
         if(newValue !== oldValue) {
             angularPlayer.adjustVolumeSlider(newValue);
         }
+    });
+
+    $scope.$on('angularPlayer:ready', function(){
+        $localStorage.playlist.forEach(function(item,index){
+            angularPlayer.addTrack(item);
+        });
+    });
+
+    $scope.$on('player:playlist', function(event, data){
+            $localStorage.playlist = data;
     });
 }]);
 
